706a6de9378344e172235c591553bf7d443f0581
Kayley Bannister <kayley.bannister@gmail.com>
2019-10-12 18:54:51 -0400

    put scores in same div with images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 // Has to be imported for every JS file
 import React from "react";
 import Images from "./components/Images";
-import Scores from "./components/Scores";
 
 function App() {
   return <Images />;
-  return <Scores />;
-
 }
 
 export default App;
--- src/components/Images.js
+++ src/components/Images.js
@@ -5,6 +5,7 @@ function Images() {
     <div>
       <h2>Images Will Be Shown Below</h2>
     <img className="image-group" alt=""></img>
+    <p>Your Score Will Be Here</p>
     </div>
   );
 }
